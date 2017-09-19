5439689b7b33407a0b5b9dbbe531b1360c3f34c7
Shaquilla Singh <shaquilla.singh@gmail.com>
2017-09-18 22:32:14 -0400

    part 2

---
+++ assets/scripts.js
@@ -0,0 +1,9 @@
+
+  $(document).ready(function(){
+    $("#sticky-header").sticky({topSpacing:0, wrapperClassName:"header-wrapper"});
+});
+
+
+  $(document).ready(function(){
+    $(".back-to-article").sticky({topSpacing:100, wrapperClassName:"back-to-article-wrapper"});
+  });
